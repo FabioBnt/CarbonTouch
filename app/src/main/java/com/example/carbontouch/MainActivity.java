e3101b3c394275dc6119c707b2037d46be3edc8f
Fabio <92687822+FabioBnt@users.noreply.github.com>
2023-03-24 09:46:36 +0100

    WIP Menu

--- app/src/main/java/com/example/carbontouch/MainActivity.java
+++ app/src/main/java/com/example/carbontouch/MainActivity.java
@@ -3,11 +3,34 @@ package com.example.carbontouch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Generate the menu of the activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    //Handle the menu item click
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.language) {
+            // TODO: change the language of the app
+            return true;
+        } else if (id == R.id.options_menu) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
